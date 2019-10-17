3b594fad6b3578f462fbdac831679d409fa4c1f4
kaanguru <kaanguru@mail.ru>
2019-10-17 15:41:21 +0300

    bulma eklendi

--- bakim-test.js
+++ bakim-test.js
@@ -1,3 +1,25 @@
+"use strict";
+
+describe("Bir gurup suit", function() {
+    it("Bir özellik tanımlaması", function() {
+      expect(true).toBe(true);
+    });
+  });
+describe("Bir gurup suit", function() {
+    it("Bir özellik tanımlaması", function() {
+      expect(true).toBe(true);
+    });
+  });
+describe("Bir gurup suit", function() {
+    it("Bir özellik tanımlaması", function() {
+      expect(true).toBe(true);
+    });
+  });
+describe("Bir gurup suit", function() {
+    it("Bir özellik tanımlaması", function() {
+      expect(true).toBe(true);
+    });
+  });
 describe("Bir gurup suit", function() {
     it("Bir özellik tanımlaması", function() {
       expect(true).toBe(true);
